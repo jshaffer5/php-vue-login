eaecfbaf2a776b6bc12d19aec60cc552f522f464
js <joshtwoone@gmail.com>
2019-02-14 01:04:18 -0800

    fuck css fuuuuuckckckkck

--- list.php
+++ list.php
@@ -9,21 +9,21 @@ session_start();
 <head>
 	<title>My List</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 <div class="container">
-	<h1 class="page-header text-center">Vue.js Todo List</h1>
+	<h1 class="page-header">Vue.js Todo List</h1>
 	
-    <div id="list" style="text-align:center;">
-        <br><br>
+    <div id="list" class="todo-list">
         <h2>
-            Todo List
+            Todos
         </h2>
-        <input placeholder="new list item" v-model="newItem" v-on:keyup="keymonitor">
-        <button v-on:click="addItem">
+        <input placeholder="new list item" v-model="newItem" v-on:keyup="keymonitor"><br>
+        <button id="add" class="btn btn-success" v-on:click="addItem">
             Add
-        </button>
-        <ul style="list-style:none">
+        </button><hr>
+        <ul id="list-items"> 
             <li v-for="todo in todos">
             {{ todo.text }}
             </li>
