4c58736bd83ed8128ddf80cdb8d34bee4514b32a
js <joshtwoone@gmail.com>
2019-03-05 17:54:58 -0800

    fixed read.php db credentials for cleardb access

--- read.php
+++ read.php
@@ -1,6 +1,16 @@
 <?php 
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+$conn = new mysqli($server, $username, $password, $db);
+
+if (!isset($conn)||(trim ($conn) == '')){
 $conn = new mysqli("localhost", "root", "root", "vue_todolist");
+}
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
